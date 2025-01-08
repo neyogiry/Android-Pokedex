41ca4ac592dc87cd69e25ec1bdfcc682cc0f8847
neyogiry <8197869+neyogiry@users.noreply.github.com>
2025-01-07 22:26:35 -0500

    style: Migrate Icon composable from Material to Material3

--- app/src/main/java/com/neyogiry/android/sample/pokedex/ui/detail/PokemonDetails.kt
+++ app/src/main/java/com/neyogiry/android/sample/pokedex/ui/detail/PokemonDetails.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
