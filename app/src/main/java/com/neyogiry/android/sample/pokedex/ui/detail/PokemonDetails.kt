d92e2e3d4ac315408f89de68e48126654d92fabe
neyogiry <8197869+neyogiry@users.noreply.github.com>
2025-01-09 22:26:01 -0500

    refactor: Replace ConstraintLayout with Column in PokemonDetails view

--- app/src/main/java/com/neyogiry/android/sample/pokedex/ui/detail/PokemonDetails.kt
+++ app/src/main/java/com/neyogiry/android/sample/pokedex/ui/detail/PokemonDetails.kt
@@ -11,15 +11,11 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.neyogiry.android.sample.pokedex.R
 import com.neyogiry.android.sample.pokedex.domain.Pokemon
@@ -61,29 +57,21 @@ private fun PokemonDetailContent(
         modifier = Modifier
             .background(color = pokemonColor)
     ) {
-        val headerLayoutId = "header"
-        val imageLayoutId = "image"
-        val detailsLayoutId = "details"
-        val constraints = constraints(headerLayoutId, imageLayoutId, detailsLayoutId)
-
-        ConstraintLayout(constraintSet = constraints, modifier = Modifier.fillMaxSize()) {
-            Header(layoutId = headerLayoutId, onBackPressed = onBackPressed)
-            Details(pokemon = pokemon, layoutId = detailsLayoutId)
-            PokemonImage(imageUrl = ImageHelper.pokemonImageUrl(url), layoutId = imageLayoutId)
+        Column(modifier = Modifier.fillMaxSize(),) {
+            Header(onBackPressed = onBackPressed)
+            Details(pokemon = pokemon, url = ImageHelper.pokemonImageUrl(url))
         }
     }
 }
 
 @Composable
 fun Header(
-    layoutId: String,
     onBackPressed: () -> Unit,
 ) {
     Box(
         modifier = Modifier
             .height(270.dp)
             .fillMaxWidth()
-            .layoutId(layoutId)
     ) {
         Icon(
             painter = painterResource(id = R.drawable.ic_expand_more),
@@ -97,30 +85,25 @@ fun Header(
 
 @Composable
 fun PokemonImage(
+    modifier: Modifier = Modifier,
     imageUrl: String,
-    layoutId: String,
 ) {
     Image(
         url = imageUrl,
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .layoutId(layoutId),
+        modifier = modifier
+            .size(200.dp)
     )
 }
 
 @Composable
 fun PokemonImage(
     imageUrl: String,
-    layoutId: String,
     headerBackgroundColor: (Color) -> Unit,
 ) {
     Image(
         url = imageUrl,
         modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .layoutId(layoutId),
+            .size(200.dp),
         averageColor = { headerBackgroundColor(it) }
     )
 }
@@ -128,22 +111,24 @@ fun PokemonImage(
 @Composable
 fun Details(
     pokemon: PokemonDetail,
-    layoutId: String,
+    url: String,
 ) {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(
-            color = Color.White,
-            shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp)
-        )
-        .layoutId(layoutId),
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(20.dp)
+            )
     ) {
         Column(
             modifier = Modifier
                 .wrapContentSize()
-                .padding(top = 60.dp, start = 24.dp, end = 14.dp, bottom = 24.dp),
+                .offset(y = (-150).dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ){
+            PokemonImage(imageUrl = url)
             Text(
                 text = pokemon.name,
                 fontSize = 24.sp,
@@ -202,39 +187,6 @@ fun Details(
         }
     }
 
-}
-
-private fun constraints(
-    headerLayoutId: Any,
-    imageLayoutId: Any,
-    detailsLayoutId: Any,
-) : ConstraintSet {
-    return ConstraintSet {
-        val header = createRefFor(headerLayoutId)
-        val image = createRefFor(imageLayoutId)
-        val details = createRefFor(detailsLayoutId)
-
-        constrain(header) {
-            top.linkTo(parent.top)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }
-
-        constrain(details) {
-            top.linkTo(header.bottom, margin = -40.dp)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }
-
-        constrain(image) {
-            linkTo(start = parent.start, top = parent.top, end = parent.end, bottom = details.top, topMargin = 12.dp, bottomMargin = (-100).dp, verticalBias = 0f)
-            width = Dimension.percent(.6f)
-            height = Dimension.fillToConstraints
-
-            centerHorizontallyTo(details)
-        }
-
-    }
 }
 
 @Preview
